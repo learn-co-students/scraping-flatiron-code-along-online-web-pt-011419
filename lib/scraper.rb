e821c6cb6b988b30fe98872c45c274b27be99a21
A Glazer <avigayilglazer@gmail.com>
2019-02-21 18:15:18 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,7 +35,12 @@ class Scraper
   end
 
   def make_courses
-
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
 
   def print_courses
@@ -43,4 +48,4 @@ class Scraper
   end
 end
 
-Scraper.new.get_page
+Scraper.new.print_courses
