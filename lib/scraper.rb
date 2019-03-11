d0c35049743078f1b619ba868728fb7246a68b9a
Ryan Mui <reachryanmui@gmail.com>
2019-03-11 00:47:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
   def print_courses
     self.make_courses
     Course.all.each do |course|
-      if course.title
+      if course.title != ""
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
@@ -37,4 +37,5 @@ end
 
 
 
-Scraper.new.get_page
+# Scraper.new.get_page
+Scraper.new.print_courses
